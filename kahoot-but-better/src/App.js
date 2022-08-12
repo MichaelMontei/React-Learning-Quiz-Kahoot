24ee62ac9dc9eadbd3fd50e1673811ec7f80cf27
MichaelMontei <code-me@hotmail.com>
2022-08-12 11:25:52 +0200

    Added logic to get the right answers and logic so we can get to the next question, when 10 questions are asked we show the score

--- kahoot-but-better/src/App.js
+++ kahoot-but-better/src/App.js
@@ -5,12 +5,15 @@ import Questions from "./Components/Questions";
 import Header from "./Components/Header";
 
 //API url
-const api_url = 'https://opentdb.com/api.php?amount=10&category=14&difficulty=easy';
+const api_url = 'https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple';
 
 function App() {
 
     //Let's make a useState with an empty array
     const [questions, setQuestions] = useState([]);
+    const [currentIndex, setcurrentIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showAnswers, setshowAnswers] = useState(false);
 
     //Let's fetch the data from tha API and set questions to hold all to the data.
     useEffect(() => {
@@ -23,15 +26,32 @@ function App() {
     }, [])
 
     const handleAnswer = (answer) => {
-        //bla bla
+        if(!showAnswers) { //prevent double answers
+            if (answer === questions[currentIndex].correct_answer) {
+                setScore(score + 1);
+            }
+        }
+       setshowAnswers(true);
+    };
+
+    const handleNextQuestion = () => {
+        setshowAnswers(false);
+        setcurrentIndex(currentIndex + 1);
     }
 
     //We got our data stored inside the State questions, so now we can check the API in our console.log to get our question and answer by going in the index and get the .question .correct_answer etc...
     return questions.length > 0 ? (
-    <div className="container">
-        <Questions data={questions[0]} handleAnswer={handleAnswer}/>
-    </div>
-    ) : (
+        <div className="container">
+            {currentIndex >= questions.length ? (
+        <h1  className="text-3xl font-bold">Your Score is: {score}!</h1>
+                ) : (
+                    <Questions data={questions[currentIndex]}
+                               showAnswers={showAnswers}
+                               handleNextQuestion={handleNextQuestion}
+                               handleAnswer={handleAnswer}/>
+                )}
+        </div>
+        ): (
         <h1 className="text-3xl text-white font-bold">Loading...</h1>
     )
 }
--- kahoot-but-better/src/Components/Questions.js
+++ kahoot-but-better/src/Components/Questions.js
@@ -1,29 +1,32 @@
 import React from "react";
 
-//We make a button with prop answer and pass it through
-const Button = ({answer}) => (
-    <button className='bg-white p-4 text-black font-semi-bold rounded shadow mb-1'>{answer}</button>
-);
-
 //The output expects data from the fetch, either a question, correct_answer or an incorrect_answer
 //We will need to know which button was clicked, and we need to shuffle the correct answer otherwise it will be always on the left top corner
 //We can shuffle elements in an array by using: https://flaviocopes.com/how-to-shuffle-array-javascript/
 
-const outputQuestions = ({ handleAnswer, data : { question, correct_answer, incorrect_answers },}) => {
+const outputQuestions = ({ showAnswers, handleAnswer, handleNextQuestion, data : { question, correct_answer, incorrect_answers },}) => {
     const shuffleAnswer = [correct_answer, ...incorrect_answers].sort(() => Math.random() -0.5);
 
 return (
-    <div>
+    <div className='flex flex-col'>
         <div className='bg-white text-black p-10 rounded-lg shadow-md'>
             <h2 className='text-2xl' dangerouslySetInnerHTML={{ __html: question}}/>
         </div>
 
         <div className='grid grid-cols-2 gap-6 mt-4'>
-            <Button onClick={() => handleAnswer(shuffleAnswer[0])} answer={shuffleAnswer[0]}/>
-            <Button onClick={() => handleAnswer(shuffleAnswer[1])} answer={shuffleAnswer[1]}/>
-            <Button onClick={() => handleAnswer(shuffleAnswer[2])} answer={shuffleAnswer[2]}/>
-            <Button onClick={() => handleAnswer(shuffleAnswer[3])} answer={shuffleAnswer[3]}/>
+            {shuffleAnswer.map(answer => {
+                const bgColor = showAnswers ? answer === correct_answer ? 'bg-green-300' : 'bg-white' : 'bg-white';
+                return (
+
+                    <button className={`${bgColor} p-6 text-black font-semi-bold rounded shadow`}
+                            onClick={() => handleAnswer(answer)} dangerouslySetInnerHTML={{__html: answer}}/>
+                );
+            })}
          </div>
+        {showAnswers && (
+                     <button    onClick={handleNextQuestion}
+                                className={`bg-blue-900 white p-4 rounded-lg shadow-md mt-6 ml-auto`}>Next Question</button>
+            )}
     </div>
 )};
 
